fdfda8b15064fe57a6b464ca7ec336b501a6f52c
Lucrezia Laraspata <l.laraspata3@phd.uniba.it>
2025-09-06 01:17:07 +0200

    reproduced spare results

--- scripts/avg_acc.py
+++ scripts/avg_acc.py
@@ -5,7 +5,13 @@ import argparse
 
 
 def get_results(model_name, data_name):
-    if model_name == "Llama-3.1-8B":
+    if model_name == "Meta-Llama-3-8B":
+        use_context_files = [
+            f"Meta-Llama-3-8B-{data_name}-12,13,14,15-0.07-2.0-grouped_activations-mutual_information-True-True-42"
+        ]
+        use_parameter_files = use_context_files
+
+    elif model_name == "Llama-3.1-8B":
         use_context_files = [
             f"Llama-3.1-8B-{data_name}-13,14,15,16-0.07-2.0-grouped_activations-mutual_information-True-True-42",
             f"Llama-3.1-8B-{data_name}-13,14,15,16-0.07-2.0-grouped_activations-mutual_information-True-True-43",
@@ -14,6 +20,7 @@ def get_results(model_name, data_name):
             f"Llama-3.1-8B-{data_name}-13,14,15,16-0.07-2.0-grouped_activations-mutual_information-True-True-46",
         ]
         use_parameter_files = use_context_files
+
     elif model_name == "Llama-2-7b-hf":
         use_context_files = [
             f"Llama-2-7b-hf-{data_name}-12,13,14,15-0.07-2.0-grouped_activations-mutual_information-True-True-42",
@@ -23,6 +30,7 @@ def get_results(model_name, data_name):
             f"Llama-2-7b-hf-{data_name}-12,13,14,15-0.07-2.0-grouped_activations-mutual_information-True-True-46",
         ]
         use_parameter_files = use_context_files
+        
     elif model_name == "gemma-2-9b":
         use_context_files = [
             f"gemma-2-9b-{data_name}-23,24,25,26-0.01-3.0-grouped_activations-mutual_information-True-True-42",
@@ -51,22 +59,39 @@ def get_results(model_name, data_name):
         cur_result = json.load(open(PROJ_DIR / "spare_outputs" / file, "r"))
         use_parameter_results.append(cur_result)
 
-    use_context_em = []
+    use_context_sub_scores = []
     for cur_result in use_context_results:
-        use_context_em.append(sum(cur_result["all_sub_scores"]) / len(cur_result["all_sub_scores"]) * 100)
-    use_parameter_em = []
+        use_context_sub_scores.append(sum(cur_result["use_context_sub_scores"]) / len(cur_result["use_context_sub_scores"]) * 100)
+    use_context_org_scores = []
     for cur_result in use_parameter_results:
-        use_parameter_em.append(sum(cur_result["all_org_scores"]) / len(cur_result["all_org_scores"]) * 100)
+        use_context_org_scores.append(sum(cur_result["use_context_org_scores"]) / len(cur_result["use_context_org_scores"]) * 100)
 
-    print("use_context_em")
-    print(use_context_em)
-    print(f"avg: {np.mean(use_context_em):.2f}")
-    print(f"std: {np.std(use_context_em):.2f}")
+    use_parameter_sub_scores = []
+    for cur_result in use_context_results:
+        use_parameter_sub_scores.append(sum(cur_result["use_parameter_sub_scores"]) / len(cur_result["use_parameter_sub_scores"]) * 100)
+    use_parameter_org_scores = []
+    for cur_result in use_parameter_results:
+        use_parameter_org_scores.append(sum(cur_result["use_parameter_org_scores"]) / len(cur_result["use_parameter_org_scores"]) * 100)
 
-    print("use_parameter_em")
-    print(use_parameter_em)
-    print(f"avg: {np.mean(use_parameter_em):.2f}")
-    print(f"std: {np.std(use_parameter_em):.2f}")
+    print("use_context_sub_scores")
+    print(use_context_sub_scores)
+    print(f"avg: {np.mean(use_context_sub_scores):.2f}")
+    print(f"std: {np.std(use_context_sub_scores):.2f}")
+
+    print("use_context_org_scores")
+    print(use_context_org_scores)
+    print(f"avg: {np.mean(use_context_org_scores):.2f}")
+    print(f"std: {np.std(use_context_org_scores):.2f}")
+
+    print("use_parameter_sub_scores")
+    print(use_parameter_sub_scores)
+    print(f"avg: {np.mean(use_parameter_sub_scores):.2f}")
+    print(f"std: {np.std(use_parameter_sub_scores):.2f}")
+
+    print("use_parameter_org_scores")
+    print(use_parameter_org_scores)
+    print(f"avg: {np.mean(use_parameter_org_scores):.2f}")
+    print(f"std: {np.std(use_parameter_org_scores):.2f}")
 
 
 def main():
